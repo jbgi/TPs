de8729f13371a174968e95bba12dfb5e76b9f9fa
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2015-04-05 11:27:00 +0200

    improve Option

--- common/src/main/java/common/data/Option.java
+++ common/src/main/java/common/data/Option.java
@@ -1,43 +1,65 @@
 package common.data;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
+
+import common.Unit;
 
 public abstract class Option<A> {
 
     // Option <A> = QuelqueChose <A> | Rien
 
-    abstract <R> R match(Function<A, R> something, Supplier<R> nothing);
+    public abstract <R> R match(Function<A, R> something, Supplier<R> nothing);
 
-    public static void main(final String[] args) {
-        final Option<Integer> o1 = something(1);
-        final Option<Integer> o2 = nothing();
-        System.out.println(afficher(o1));
-        System.out.println(afficher(o2));
+    public <U> Option<U> map(final Function<A, U> mapper) {
+        return this.match(
+                (final A a) -> Option.something(mapper.apply(a)),
+                () -> Option.<U> nothing());
+    }
+
+    public void forEach(final Consumer<A> consummer) {
+        this.match(
+                (final A a) -> {
+                    consummer.accept(a);
+                    return Unit.unit();
+                },
+                Unit::unit);
+    }
+
+    public A orElse(final A other) {
+        return match(
+                a -> a,
+                () -> other);
     }
 
     public static <A> Option<A> something(final A a) {
-        return new Option<A>() {
-            @Override
-            <R> R match(final Function<A, R> something, final Supplier<R> nothing) {
-                return something.apply(a);
-            }
-        };
+        return new Something<>(a);
     }
 
     public static <A> Option<A> nothing() {
-        return new Option<A>() {
-            @Override
-            <R> R match(final Function<A, R> something, final Supplier<R> nothing) {
-                return nothing.get();
-            }
-        };
+        return new Nothing<>();
     }
 
-    static String afficher(final Option<Integer> os) {
-        return os.match(
-                (Integer i) -> "Something(" + i + ")",
-                () -> "Nothing()");
+    private static final class Nothing<A> extends Option<A> {
+        @Override
+        public <R> R match(final Function<A, R> something, final Supplier<R> nothing) {
+            return nothing.get();
+        }
+    }
+
+    private static final class Something<A> extends Option<A> {
+
+        private final A a;
+
+        private Something(final A a) {
+            this.a = a;
+        }
+
+        @Override
+        public <R> R match(final Function<A, R> something, final Supplier<R> nothing) {
+            return something.apply(a);
+        }
 
     }
 }
